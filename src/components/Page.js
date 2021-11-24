81b8cb7d82eee1588e86b3766be4d8cc3b7a1fb5
Fatih Delice <2017141044@cumhuriyet.edu.tr>
2021-11-24 15:02:58 +0300

    updated

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,7 +5,7 @@ export default function Page() {
         <section>
             <div className="page-container">
                 <h1>3 Adımda Mükemmel Bir CV Oluştur</h1>
-                <div className="page-card">                    
+                <div className="page-card" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">                    
                 <img src="https://pngimg.com/uploads/cv/cv_PNG38.png" className="page-icons"></img>
                     <h2>
                         CV formunuzu doldurun 
@@ -14,7 +14,7 @@ export default function Page() {
                         CVdown sitemize hızlı ve kolay bir şekilde üye olarak CV formunuzu doldurun.
                     </p>
                 </div>
-                <div className="page-card">
+                <div className="page-card" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.4s">
                     <img src="https://cdn-icons-png.flaticon.com/512/1939/1939574.png" className="page-icons"></img>
                     <h2>
                         CV şablonunuzu seçin
@@ -23,7 +23,7 @@ export default function Page() {
                         Kendinizi en iyi bir şekilde anımsatan şablonunuzu seçin.
                     </p>
                 </div>
-                <div className="page-card">
+                <div className="page-card" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
                     <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698392-icon-129-cloud-download-512.png" className="page-icons"></img>
                     <h2>
                         CV'nizi indirin
