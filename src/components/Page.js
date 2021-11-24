c2a5f1b92e1252ccee6aaffb357fdc3f61192907
yucellsarikaya <2017141050@cumhuriyet.edu.tr>
2021-11-24 21:56:58 +0300

    animations

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,37 +1,64 @@
 import React, { useState } from 'react'
+import '../PageAnimation.scss'
 
 export default function Page() {
     return (
         <section>
             <div className="page-container">
                 <h1>3 Adımda Mükemmel Bir CV Oluştur</h1>
-                <div className="page-card">                    
-                <img src="https://pngimg.com/uploads/cv/cv_PNG38.png" className="page-icons"></img>
-                    <h2>
-                        CV formunuzu doldurun 
-                    </h2>
-                    <p>
-                        CVdown sitemize hızlı ve kolay bir şekilde üye olarak CV formunuzu doldurun.
-                    </p>
-                </div>
+
+
                 <div className="page-card">
-                    <img src="https://cdn-icons-png.flaticon.com/512/1939/1939574.png" className="page-icons"></img>
-                    <h2>
-                        CV şablonunuzu seçin
-                    </h2>
-                    <p>
-                        Kendinizi en iyi bir şekilde anımsatan şablonunuzu seçin.
-                    </p>
+                    <div class="card">
+                        <div class="card__face card__face--front">
+                            <img src="https://pngimg.com/uploads/cv/cv_PNG38.png" className="page-icons"></img>
+                            <h2>
+                                CV formunuzu doldurun
+                            </h2>
+                            <p>
+                                CVdown sitemize hızlı ve kolay bir şekilde üye olarak CV formunuzu doldurun.
+                            </p>
+                        </div>
+                        <div class="card__face card__face--back">
+                            <img src="https://i.loli.net/2019/11/16/cqyJiYlRwnTeHmj.jpg" className="page_cart__img" />
+                        </div>
+                    </div>
                 </div>
+
                 <div className="page-card">
-                    <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698392-icon-129-cloud-download-512.png" className="page-icons"></img>
-                    <h2>
-                        CV'nizi indirin
-                    </h2>
-                    <p>
-                        Cv'nizi indirip. İndirme işlemi tamamlandığında CV'nizi görüntüleyebilirsiniz.
-                    </p>
+                    <div class="card">
+                        <div class="card__face card__face--front">
+                            <img src="https://cdn-icons-png.flaticon.com/512/1939/1939574.png" className="page-icons"></img>
+                            <h2>
+                                CV şablonunuzu seçin
+                            </h2>
+                            <p>
+                                Kendinizi en iyi bir şekilde anımsatan şablonunuzu seçin.
+                            </p>
+                        </div>
+                        <div class="card__face card__face--back">
+                            <img src="https://i.loli.net/2019/11/16/cqyJiYlRwnTeHmj.jpg" className="page_cart__img" />
+                        </div>
+                    </div>
                 </div>
+
+                <div className="page-card">
+                    <div class="card">
+                        <div class="card__face card__face--front">
+                            <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698392-icon-129-cloud-download-512.png" className="page-icons"></img>
+                            <h2>
+                                CV'nizi indirin
+                            </h2>
+                            <p>
+                                Cv'nizi indirip. İndirme işlemi tamamlandığında CV'nizi görüntüleyebilirsiniz.
+                            </p>
+                        </div>
+                        <div class="card__face card__face--back">
+                            <img src="https://i.loli.net/2019/11/16/cqyJiYlRwnTeHmj.jpg" className="page_cart__img" />
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </section>
     )
